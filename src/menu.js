7f1b6050ef6159e7a3d7bf65bf7309035353d77e
Brandon Thai <brandon88thai@hotmail.com>
2023-02-16 15:37:37 -0500

    Added eslint to project

--- src/menu.js
+++ src/menu.js
@@ -1,41 +1,41 @@
-import './style.css';
-import generateElement from './generateElement.js';
-import makeHeader from './makeHeader.js';
-import coffeeImage1 from './latteArt.jpg';
+import './style.css'
+import generateElement from './generateElement.js'
+import makeHeader from './makeHeader.js'
+import coffeeImage1 from './latteArt.jpg'
 
-function addItem(imgSrc, imgClass, imgAlt, itemName, itemDesc) {
-    let item = generateElement('div', 'item', '');
+function addItem (imgSrc, imgClass, imgAlt, itemName, itemDesc) {
+  const item = generateElement('div', 'item', '')
 
-    let img = new Image();
-    img.classList.add(imgClass);
-    img.src = imgSrc;
-    img.alt = imgAlt;
-    item.appendChild(img);
+  const img = new Image()
+  img.classList.add(imgClass)
+  img.src = imgSrc
+  img.alt = imgAlt
+  item.appendChild(img)
 
-    let name = generateElement('h2', '', itemName);
-    item.appendChild(name);
+  const name = generateElement('h2', '', itemName)
+  item.appendChild(name)
 
-    let desc = generateElement('p', '', itemDesc);
-    item.appendChild(desc);
+  const desc = generateElement('p', '', itemDesc)
+  item.appendChild(desc)
 
-    return item;
+  return item
 }
 
-function menu() {
-    let m = generateElement('div', 'menu', '');
-    m.id = 'main';
+function menu () {
+  const m = generateElement('div', 'menu', '')
+  m.id = 'main'
 
-    let item1 = addItem(coffeeImage1, 'round', 'latte art', 'Latte', 'Organic Coffee, Your choice of milk, sugar');
+  const item1 = addItem(coffeeImage1, 'round', 'latte art', 'Latte', 'Organic Coffee, Your choice of milk, sugar')
 
-    m.appendChild(item1);
+  m.appendChild(item1)
 
-    return m;
+  return m
 }
 
-export default function menuPage() {
-    let div = document.createElement('div');
-    div.id = 'content';
-    div.appendChild(makeHeader('menu'));
-    div.appendChild(menu());
-    return div;
+export default function menuPage () {
+  const div = document.createElement('div')
+  div.id = 'content'
+  div.appendChild(makeHeader('menu'))
+  div.appendChild(menu())
+  return div
 }
